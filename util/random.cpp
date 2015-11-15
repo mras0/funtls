467e674ba1018bef44b7faaed6f1ab51b9943e44
Michael Rasmussen <michaelrasmussen1337@gmail.com>
2015-11-15 18:29:34 +0100

    Use std::random_device on Linux.

--- util/random.cpp
+++ util/random.cpp
@@ -1,33 +1,13 @@
 #include "random.h"
-#include <fstream>
-#include <stdexcept>
-
-#ifdef WIN32
 #include <random>
-#endif
 
 namespace funtls { namespace util {
 
+// TODO: FIXME: XXX: This isn't secure or smart
 void get_random_bytes(void* dest, size_t count) {
-#ifdef WIN32
 	auto d = reinterpret_cast<uint8_t*>(dest);
-	while (count--) *d++ = static_cast<uint8_t>(std::random_device()());
-#elif 1
-    std::ifstream urandom("/dev/urandom", std::ifstream::binary);
-    if (!urandom || !urandom.is_open()) {
-        throw std::runtime_error("Could not open /dev/urandom");
-    }
-    if (!urandom.read(reinterpret_cast<char*>(dest), count)) {
-        throw std::runtime_error("Could not read from /dev/urandom");
-    }
-#else
-    // "random" but reproducable results
-    static uint8_t seed = 0;
-    uint8_t* b = static_cast<uint8_t*>(dest);
-    while (count--) {
-        *b++ = seed++;
-    }
-#endif
+    std::random_device rd{};
+	while (count--) *d++ = static_cast<uint8_t>(rd());
 }
 
 } } // namespace funtls::util
