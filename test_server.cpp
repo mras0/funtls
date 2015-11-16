211c9e8d0eb595e1249368a0caceb1fee9b86a35
Michael Rasmussen <michaelrasmussen1337@gmail.com>
2015-11-16 23:24:52 +0100

    Rename (server) connection to connection_to_client

--- test_server.cpp
+++ test_server.cpp
@@ -52,24 +52,24 @@ public:
 
 using client_connect_handler = std::function<void(util::async_result<std::shared_ptr<tls_base>>)>;
 
-class connection : public tls_base, public std::enable_shared_from_this<connection> {
+class connection_to_client : public tls_base, public std::enable_shared_from_this<connection_to_client> {
 public:
-    using ptr_t = std::shared_ptr<connection>;
+    using ptr_t = std::shared_ptr<connection_to_client>;
 
     static ptr_t make(const std::string& name, std::unique_ptr<stream> stream, const std::vector<const server_id*> server_ids, const client_connect_handler& on_client_connected) {
-        auto c = ptr_t(new connection{name, std::move(stream), server_ids, on_client_connected});
+        auto c = ptr_t(new connection_to_client{name, std::move(stream), server_ids, on_client_connected});
         c->read_client_hello();
         return c;
     }
 
-    ~connection();
+    ~connection_to_client();
 private:
     util::ostream_adapter                           log_;
     std::vector<const server_id*>                   server_ids_;
     client_connect_handler                          on_client_connected_;
     std::unique_ptr<server_key_exchange_protocol>   server_kex_;
 
-    connection(const std::string& name, std::unique_ptr<stream> stream, const std::vector<const server_id*> server_ids, const client_connect_handler& on_client_connected);
+    connection_to_client(const std::string& name, std::unique_ptr<stream> stream, const std::vector<const server_id*> server_ids, const client_connect_handler& on_client_connected);
 
     void read_client_hello();
     void send_server_hello();
@@ -81,7 +81,7 @@ private:
     void handle_error(std::exception_ptr e) const;
 };
 
-connection::connection(const std::string& name, std::unique_ptr<stream> stream, const std::vector<const server_id*> server_ids, const client_connect_handler& on_client_connected)
+connection_to_client::connection_to_client(const std::string& name, std::unique_ptr<stream> stream, const std::vector<const server_id*> server_ids, const client_connect_handler& on_client_connected)
     : tls_base(std::move(stream), tls_base::connection_end::server)
     , log_([name] (const std::string& s) { std::cout << name + ": " + s;} )
     , server_ids_(server_ids)
@@ -92,12 +92,12 @@ connection::connection(const std::string& name, std::unique_ptr<stream> stream, 
     assert(!server_ids_.empty());
 }
 
-connection::~connection()
+connection_to_client::~connection_to_client()
 {
     log_ << "Conncetion dropped" << std::endl;
 }
 
-void connection::handle_error(std::exception_ptr e) const
+void connection_to_client::handle_error(std::exception_ptr e) const
 {
     assert(e);
     std::ostringstream msg;
@@ -114,7 +114,7 @@ void connection::handle_error(std::exception_ptr e) const
 }
 
 
-void connection::read_client_hello() {
+void connection_to_client::read_client_hello() {
     log_ << "Reading ClientHello" << std::endl;
     auto self = shared_from_this();
     read_handshake(wrapped(
@@ -192,10 +192,10 @@ void connection::read_client_hello() {
 
                 self->send_server_hello();
             },
-            std::bind(&connection::handle_error, self, std::placeholders::_1)));
+            std::bind(&connection_to_client::handle_error, self, std::placeholders::_1)));
 }
 
-void connection::send_server_hello()
+void connection_to_client::send_server_hello()
 {
     log_ << "Sending ServerHello" << std::endl;
     auto self = shared_from_this();
@@ -209,10 +209,10 @@ void connection::send_server_hello()
             std::vector<extension>{}
         }), wrapped([self] () {
             self->send_server_certificate();
-        }, std::bind(&connection::handle_error, self, std::placeholders::_1)));
+        }, std::bind(&connection_to_client::handle_error, self, std::placeholders::_1)));
 }
 
-void connection::send_server_certificate()
+void connection_to_client::send_server_certificate()
 {
     const auto chain = server_kex_->certificate_chain();
     if (chain) {
@@ -221,36 +221,36 @@ void connection::send_server_certificate()
         send_handshake(make_handshake(
             certificate{ *chain }), wrapped([self] () {
                 self->send_server_key_exchange();
-            }, std::bind(&connection::handle_error, self, std::placeholders::_1)));
+            }, std::bind(&connection_to_client::handle_error, self, std::placeholders::_1)));
     } else {
         send_server_key_exchange();
     }
 }
 
-void connection::send_server_key_exchange()
+void connection_to_client::send_server_key_exchange()
 {
     if (auto handshake = server_kex_->server_key_exchange(client_random(), server_random())) {
         log_ << "Sending ServerKeyExchange" << std::endl;
         auto self = shared_from_this();
         send_handshake(*handshake, wrapped([self]() {
             self->send_server_hello_done();
-        }, std::bind(&connection::handle_error, self, std::placeholders::_1)));
+        }, std::bind(&connection_to_client::handle_error, self, std::placeholders::_1)));
     } else {
         send_server_hello_done();
     }
 }
 
-void connection::send_server_hello_done()
+void connection_to_client::send_server_hello_done()
 {
     log_ << "Sending ServerHelloDone" << std::endl;
     auto self = shared_from_this();
     send_handshake(make_handshake(
         server_hello_done{}), wrapped([self] () {
             self->read_client_key_exchange();
-        }, std::bind(&connection::handle_error, self, std::placeholders::_1)));
+        }, std::bind(&connection_to_client::handle_error, self, std::placeholders::_1)));
 }
 
-void connection::read_client_key_exchange() {
+void connection_to_client::read_client_key_exchange() {
     log_ << "Reading ClientKeyExchange" << std::endl;
     auto self = shared_from_this();
     read_handshake(wrapped(
@@ -267,11 +267,11 @@ void connection::read_client_key_exchange() {
                                         self->log_ << "Handshake done. Session id " << util::base16_encode(self->session_id().as_vector()) << std::endl;
                                         self->on_client_connected_(std::shared_ptr<tls_base>{self});
                                     },
-                                    std::bind(&connection::handle_error, self, std::placeholders::_1)));
+                                    std::bind(&connection_to_client::handle_error, self, std::placeholders::_1)));
                             },
-                            std::bind(&connection::handle_error, self, std::placeholders::_1)));
+                            std::bind(&connection_to_client::handle_error, self, std::placeholders::_1)));
             },
-            std::bind(&connection::handle_error, self, std::placeholders::_1)));
+            std::bind(&connection_to_client::handle_error, self, std::placeholders::_1)));
 }
 
 class rsa_server_id : public server_id {
@@ -1015,7 +1015,7 @@ int main(int argc, char* argv[])
                 } else {
                     std::ostringstream oss;
                     oss << state->socket.remote_endpoint();
-                    tls::connection::make(oss.str(), make_tls_stream(std::move(state->socket)), {&server_id}, &main_loop);
+                    tls::connection_to_client::make(oss.str(), make_tls_stream(std::move(state->socket)), {&server_id}, &main_loop);
                 }
                 start_accept();
             });
